de1fdd02e811b0158eefd21efc59fcff59bc6498
shigure2657 <cloudcompute2657@gmail.com>
2025-10-21 15:19:01 +0900

    Updates

--- next.config.js
+++
@@ -1,8 +0,0 @@
-const isProd = process.env.NODE_ENV === 'production';
-
-module.exports = {
-  output: "export",
-  assetPrefix: isProd ? '/mysite/' : '',
-  basePath: isProd ? '/mysite' : '',
-  trailingSlash: true,
-};
