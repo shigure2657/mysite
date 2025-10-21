613f33d9698e69f3fe09cb3aab8cda99d3929ee6
shigure2657 <cloudcompute2657@gmail.com>
2025-10-21 14:23:26 +0900

    Deploy to GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 const isProd = process.env.NODE_ENV === 'production';
 
 module.exports = {
+  output: "export",
   assetPrefix: isProd ? '/mysite/' : '',
   basePath: isProd ? '/mysite' : '',
   trailingSlash: true,
